900151aa2568b136a6b1c48c6bfde00b68dcf031
Dan Prince <1266011+danprince@users.noreply.github.com>
2023-08-30 00:53:12 +0100

    experimental build summary in github action

--- scripts/measure.sh
+++ scripts/measure.sh
@@ -5,3 +5,25 @@ max_size=13312 # (13 * 1024)
 size=$(ls -l "$file" | awk '{ print $5 }')
 percent=$(echo "$size / $max_size * 100" | bc -l)
 printf "%d/%d bytes (%.2f%%)\n" "$size" "$max_size" "$percent"
+
+build_summary() {
+  printf "# Build "
+
+  if (( size < max_size )); then
+    printf "✅"
+  else
+    printf "❌"
+  fi
+
+  printf "\n\n"
+
+  printf "\`\`\`mermaid\n"
+  printf "pie showData\n"
+  printf "    \"Used (bytes)\" : %d\n" "$size"
+  printf "    \"Free (bytes)\" : %d\n" "$((max_size - size))"
+  printf "\`\`\`\n"
+}
+
+if [[ -n "$GITHUB_STEP_SUMMARY" ]]; then
+  build_summary > "$GITHUB_STEP_SUMMARY"
+fi
