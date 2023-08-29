5550eaaf9a09cd8175786781767eb797c7ce623a
Dan Prince <1266011+danprince@users.noreply.github.com>
2023-08-29 07:23:46 +0100

    adds viewport culling

--- src/geometry.ts
+++ src/geometry.ts
@@ -60,3 +60,12 @@ export function getPointOnCircle(
     y: y + Math.cos(angle) * radius,
   }
 }
+
+export function isPointInRect(point: Point, rect: Rectangle): boolean {
+  return (
+    point.x >= rect.x1 &&
+    point.y >= rect.y1 &&
+    point.x <= rect.x2 &&
+    point.y <= rect.y2
+  );
+}
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,5 +1,6 @@
 import { clear, ctx, drawSprite } from "./engine";
 import { game } from "./game";
+import { isPointInRect } from "./geometry";
 import * as sprites from "./sprites";
 
 export function render() {
@@ -10,6 +11,10 @@ export function render() {
   ctx.translate(-view.x1, -view.y1);
 
   for (let unit of game.units) {
+    if (!isPointInRect(unit, view)) {
+      continue;
+    }
+
     drawSprite(sprites.shadow, unit.x, unit.y);
 
     if (unit.mount) {
