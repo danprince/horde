2760fe5929ef7d44b8cc30a0dfe1d2b7f66f8b14
Dan Prince <1266011+danprince@users.noreply.github.com>
2023-08-30 07:53:16 +0100

    customize mermaid theme variables

--- scripts/measure.sh
+++ scripts/measure.sh
@@ -10,7 +10,21 @@ printf "%s %d/%d bytes (%.2f%%)\n" "$emoji" "$size" "$max_size" "$percent"
 if [[ -n "$GITHUB_STEP_SUMMARY" ]]; then
   printf "
 \`\`\`mermaid
-%%%%{init: { 'theme': 'base', 'themeVariables': { 'pie1': '#b12a34', 'pie2': '#fff' } } }%%%%
+%%%%{
+  init: {
+    'theme': 'base',
+    'themeVariables': {
+      'pie1': '#b12a34',
+      'pie2': '#fff',
+      'pieOpacity': 1,
+      'fontFamily': 'monospace',
+      'pieStrokeWidth': '4px',
+      'pieSectionTextSize': '40px',
+      'pieTitleTextSize': '30px',
+      'pieLegendTextSize': '25px'
+    }
+  }
+}%%%%
 pie showData
     title Budget (%.2f%%) %s
     \"Used (bytes)\": %d
